24e9936b815b458df4462a6f23875d489e204082
Benjamin Zeigler <benjamin@sololegends.com>
2022-10-05 17:41:09 -0400

    Version incrementing to v0.9.3

--- src/main.ts
+++ src/main.ts
@@ -100,8 +100,8 @@ Vue.prototype.$fn = {
   }
 };
 
-window.APP_VERSION = "v0.9.1";
-window.BUILD_DATE = "2022-10-05 17:40:41";
+window.APP_VERSION = "v0.9.3";
+window.BUILD_DATE = "2022-10-05 17:41:09";
 export default createApp({
   router,
   render: (h: ((app: VueConstructor) => void)) => h(App),
