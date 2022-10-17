ed4c1235d9ea7789aae2bc1614f001c90bbb6920
Benjamin Zeigler <benjamin@sololegends.com>
2022-10-17 19:24:41 -0400

    another try?

--- src/main.ts
+++ src/main.ts
@@ -101,7 +101,7 @@ Vue.prototype.$fn = {
 };
 
 window.APP_VERSION = "v0.9.9";
-window.BUILD_DATE = "2022-10-17 19:15:20";
+window.BUILD_DATE = "2022-10-17 19:24:41";
 export default createApp({
   router,
   render: (h: ((app: VueConstructor) => void)) => h(App),
