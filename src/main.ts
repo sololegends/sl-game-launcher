8be9d9f306fec2e53424ec2688738458d4e090ad
Benjamin Zeigler <benjamin@sololegends.com>
2022-10-17 18:37:09 -0400

    Removed the yarn.lock from the ignore

--- src/main.ts
+++ src/main.ts
@@ -101,7 +101,7 @@ Vue.prototype.$fn = {
 };
 
 window.APP_VERSION = "v0.9.9";
-window.BUILD_DATE = "2022-10-17 18:35:05";
+window.BUILD_DATE = "2022-10-17 18:37:09";
 export default createApp({
   router,
   render: (h: ((app: VueConstructor) => void)) => h(App),
