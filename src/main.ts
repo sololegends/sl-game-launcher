1727be93e9c64832eb18adc4a729e89c0a2cd4a4
Benjamin Zeigler <benjamin@sololegends.com>
2022-10-17 20:03:35 -0400

    test

--- src/main.ts
+++ src/main.ts
@@ -101,7 +101,7 @@ Vue.prototype.$fn = {
 };
 
 window.APP_VERSION = "v0.9.9";
-window.BUILD_DATE = "2022-10-17 19:59:48";
+window.BUILD_DATE = "2022-10-17 20:03:35";
 export default createApp({
   router,
   render: (h: ((app: VueConstructor) => void)) => h(App),
