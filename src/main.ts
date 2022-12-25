8a92a58240c4ebc6780a60fd73b11bceb98a830c
Benjamin Zeigler <benjamin@sololegends.com>
2022-12-25 00:25:29 -0500

    ci update

--- src/main.ts
+++ src/main.ts
@@ -112,7 +112,7 @@ Vue.prototype.$app = {
 };
 
 window.APP_VERSION = "v0.10.6";
-window.BUILD_DATE = "2022-12-25 00:15:04";
+window.BUILD_DATE = "2022-12-25 00:25:29";
 export default createApp({
   router,
   render: (h: ((app: VueConstructor) => void)) => h(App),
--- upload_dist.sh
+++ upload_dist.sh
@@ -4,6 +4,7 @@ echo "Finding exe"
 exe=$(find dist_electron/ -maxdepth 1 -name 'SL*.exe')
 blockmap=$(find dist_electron/ -maxdepth 1 -name 'SL*.blockmap')
 version_v=${CI_COMMIT_TAG:-$1}
+echo "EXE: $exe, BLOCKMAP: $blockmap, version_v: $version_v"
 version=${version_v:1}
 
 echo curl -F 'app=$APP_NAME' -F "exe=@$exe" -F "blockmap=@$blockmap" -F "version=$version" $SERVER
