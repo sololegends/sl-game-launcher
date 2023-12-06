09ad8fa75355eb3f747db90f83301abe3eb1739e
Benjamin Zeigler <benjamin@sololegends.com>
2023-12-05 22:21:41 -0500

    Fixed CI mis-type

--- src/main.ts
+++ src/main.ts
@@ -118,7 +118,7 @@ Vue.prototype.$app = {
 };
 
 window.APP_VERSION = "v0.12.0";
-window.BUILD_DATE = "2023-12-05 21:50:46";
+window.BUILD_DATE = "2023-12-05 22:21:41";
 export default createApp({
   router,
   render: (h: ((app: VueConstructor) => void)) => h(App),
